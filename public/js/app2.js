dd94b970433298910a7cf46e6a3e92c7dcda26fd
Matthew Morais <moraismatthew@yahoo.com>
2017-02-04 15:14:27 -0500

    Project has been set up and js is prepped, but code does not work yet.

---
+++ public/js/app2.js
@@ -0,0 +1,14 @@
+var imgrishApp = {
+    imgurl: [],
+    init: function(){
+        imgrishApp.cacheDOM();
+        imgrishApp.addEventListeners();
+        imgrishApp.render();
+    },
+    cacheDOM: function (){},
+    addEventListeners: function(){},
+    render: function (){},
+    addToDo: function(){}
+};
+
+imgrishApp.init();
