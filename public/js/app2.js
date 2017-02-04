e5e23215c0d555cd7ed00585b4f928c2ec9f2380
Matthew Morais <moraismatthew@yahoo.com>
2017-02-04 17:47:01 -0500

    Added app2.js. Linked the html to app2.js. Then added cacheing system for all ids linked to url field, button, and the list at the bottom. Then added a rendering of img html to grab links and output them as html.

--- public/js/app2.js
+++ public/js/app2.js
@@ -5,10 +5,21 @@ var imgrishApp = {
         imgrishApp.addEventListeners();
         imgrishApp.render();
     },
-    cacheDOM: function (){},
-    addEventListeners: function(){},
-    render: function (){},
-    addToDo: function(){}
+    cacheDOM: function (){
+        imgrishApp.picture = document.querySelector('#url');
+        imgrishApp.createButton = document.querySelector('#create');
+        imgrishApp.list = document.querySelector('#list');
+    },
+    addEventListeners: function(){
+        imgrishApp.createButton.addEventListener('click', imgrishApp.addImg);
+    },
+    render: function (){
+        imgrishApp.list.innerHTML = imgrishApp.imgurl.map(url => `<li><img src="${url}"></li>`).join{''};
+    },
+    addImg: function(){
+        imgrishApp.imgurl.push (imgrishApp.picture.value);
+        imgrishApp.render();
+    }
 };
 
 imgrishApp.init();
